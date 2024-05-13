7285cb6414c08d2613d7ff33d3f52928fa7749bd
Anasmaho <161331881+Anasmaho@users.noreply.github.com>
2024-05-13 16:33:09 +0200

    Add files via upload

---
+++ code.js
@@ -0,0 +1,12 @@
+function link(site) {
+    if (site == 0) {
+        window.location.href = "main.html";
+    } else if ( site == 1) {
+        window.location.href = "Protien.html"
+    } else if (site == 2) {
+        window.location.href = "Pwo.html"
+    }  else if ( site == 3) {
+        window.location.href = "Kreatin.html"
+    }
+
+}
