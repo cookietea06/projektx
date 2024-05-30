36e90100c86a5fb386e8467b1eaf9fa5564c7ec4
Anasmaho <161331881+Anasmaho@users.noreply.github.com>
2024-05-30 17:44:54 +0200

    Update code.js

--- code.js
+++ code.js
@@ -7,6 +7,20 @@ function link(site) {
         window.location.href = "Pwo.html"
     }  else if ( site == 3) {
         window.location.href = "Kreatin.html"
+    }  else if ( site == 4) {
+        window.location.href = "pwoLW.html"
+    }  else if ( site == 5) {
+        window.location.href = "pwoC4.html"
+    }  else if ( site == 6) {
+        window.location.href = "tyngre.html"
+    }  else if ( site == 7) {
+        window.location.href = "Optimumnutrion.html"
+    }  else if ( site == 8) {
+        window.location.href = "starkreatin.html"
+    }  else if ( site == 9) {
+        window.location.href = "optimumcreatin.html"
     }
+    
+    
 
 }
